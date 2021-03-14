dd9fca9cb9dd82f6447b19e6ae769ee4c850dd71
Алексей Виноградов <vinogradov.av20@gmail.com>
2021-03-14 20:06:45 +0200

    Добавьте файлы проекта.

---
+++ socketOst/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+int main() {
+	// служебная структура для хранение информации
+	// о реализации Windows Sockets
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	// Если произошла ошибка подгрузки библиотеки
+	if (result != 0) {
+		cerr << "WSAStartup failed: " << result << "\n";
+		return result;
+	}
+
+	struct addrinfo* addr = NULL; // структура, хранящая информацию
+	// об IP-адресе  слущающего сокета
+
+	// Шаблон для инициализации структуры адреса
+	struct addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
+
+	// AF_INET определяет, что используется сеть для работы с сокетом
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM; // Задаем потоковый тип сокета
+	hints.ai_protocol = IPPROTO_TCP; // Используем протокол TCP
+	// Сокет биндится на адрес, чтобы принимать входящие соединения
+	hints.ai_flags = AI_PASSIVE;
+
+	result = getaddrinfo("62.80.183.236", "1234", &hints, &addr);
+
+	// Если инициализация структуры адреса завершилась с ошибкой,
+	// выведем сообщением об этом и завершим выполнение программы 
+	if (result != 0) {
+		cerr << "getaddrinfo failed: " << result << "\n";
+		WSACleanup(); // выгрузка библиотеки Ws2_32.dll
+		return 1;
+	}
+
+	// Создание сокета
+	int listen_socket = socket(addr->ai_family, addr->ai_socktype,
+		addr->ai_protocol);
+	// Если создание сокета завершилось с ошибкой, выводим сообщение,
+	// освобождаем память, выделенную под структуру addr,
+	// выгружаем dll-библиотеку и закрываем программу
+	if (listen_socket == INVALID_SOCKET) {
+		cerr << "Error at socket: " << WSAGetLastError() << "\n";
+		freeaddrinfo(addr);
+		WSACleanup();
+		return 1;
+	}
+
+
+	/////////////////////////////////////////////////////////////
+
+
+	// Привязываем сокет к IP-адресу
+	result = bind(listen_socket, addr->ai_addr, (int)addr->ai_addrlen);
+
+	// Если привязать адрес к сокету не удалось, то выводим сообщение
+	// об ошибке, освобождаем память, выделенную под структуру addr.
+	// и закрываем открытый сокет.
+	// Выгружаем DLL-библиотеку из памяти и закрываем программу.
+	if (result == SOCKET_ERROR) {
+		cerr << "bind failed with error: " << WSAGetLastError() << "\n";
+		freeaddrinfo(addr);
+		closesocket(listen_socket);
+		WSACleanup();
+		return 1;
+	}
+
+
+
+}
